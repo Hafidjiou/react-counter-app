c421d7f8a83e2f57dd2ad5ea8db69e1ac1e4a74c
Mamadou Diallo <bambaya.diallo123@gmail.com>
2025-06-16 10:25:09 +0200

    creation de index.html

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Counter from './components/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Application Compteur</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Compteur : {count}</p>
+      <button onClick={() => setCount(count + 1)}>Incrémenter</button>
+      <button onClick={() => setCount(count - 1)}>Décrémenter</button>
+      <button onClick={() => setCount(0)}>Réinitialiser</button>
+    </div>
+  );
+}
+
+export default Counter;
